d559182583aecfa8a6b9e7223401d0411000e506
Julian Pagtama <jpagtama@gmail.com>
2020-06-08 22:19:01 -0700

    Initial set up for project2

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,11 @@
 import React from 'react'
-import Header from './Header'
-import Body from './Body'
-import { ThemeProvider } from '@material-ui/core'
-import theme from '../assets/themes/Theme'
 
-class App extends React.Component {
-  render() {
-    return (
-      <ThemeProvider theme={theme}>
-      <Header />
-      <Body />
-      </ThemeProvider>
-    )
-  }
+const App = () => {
+  return (
+    <div>
+      Test
+    </div>
+  )
 }
 
 export default App
