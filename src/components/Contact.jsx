8c6de641b0b8269e7477b718916602db5b872116
Julian Pagtama <jpagtama@gmail.com>
2020-06-12 23:53:53 -0700

    Grid layout for Contact component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,15 +3,21 @@ import { Grid, TextField } from '@material-ui/core'
 
 const Contact = () => {
     return (
-        <Grid container spacing={2} >
-            <Grid item md={6} >
-                <TextField name="firstName" variant="outlined" label="First Name" />
+        <React.Fragment>
+            <Grid container spacing={2} style={{marginTop:"2em"}} justify="center">
+                <Grid item md={6} >
+                    <TextField name="firstName" variant="outlined" label="First Name" />
+                </Grid>
+                <Grid item md={6} >
+                    <TextField name="lastName" variant="outlined" label="Last Name" />
+                </Grid>
             </Grid>
-            <Grid item md={6} >
-                <TextField name="lastName" variant="outlined" label="Last Name" />
+            <Grid container justify="center" style={{marginTop:"1em"}}>
+                <Grid item md={12}>
+                    <TextField name="email" variant="outlined" label="Email" />
+                </Grid>
             </Grid>
-
-        </Grid>
+        </React.Fragment>
     )
 }
 
